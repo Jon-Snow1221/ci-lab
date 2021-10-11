0a89fae4a41828f70b711a3b50bef17c303aac07
Jonathan Llewellyn <jonathan.llewellyn27@gmail.com>
2021-10-11 14:14:03 -0600

    added grocery test

--- project.js
+++ project.js
@@ -3,3 +3,8 @@ function getGreeting(name){
 }
 
 module.exports = getGreeting;
+
+// function getGroceryList(string1, string2, string3) {
+//     return "Grocery list: " + string1 + string2 + string3;
+// };
+
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 const getGreeting = require('./project');
+// const getGroceryList  = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+// test('get grocery list', () => {
+//   expect(getGroceryList('Eggs', 'Milk', 'Bread')).toBe('Grocery List: EggsMilkBread');
+// });
